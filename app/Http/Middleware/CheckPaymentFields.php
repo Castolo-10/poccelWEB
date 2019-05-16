ca21fcc766e39738f320216d0a0296ef963e870c
Ricardo Peralta <rcardo.peralta@gmail.com>
2019-05-16 09:18:26 -0500

    expresiones regulares para validación de campos

--- app/Http/Middleware/CheckPaymentFields.php
+++ app/Http/Middleware/CheckPaymentFields.php
@@ -17,11 +17,11 @@ class CheckPaymentFields
     {
         $request->validate([
             'credit_card_titular' => 'required|max:100',
-            'credit_card_number' => 'required|size:16|regex:/\d.+/i',
-            'credit_card_expiration_date' => 'required|size:7|date_format:Y-m',
-            'credit_card_security_code' => 'required|size:3|regex:/\d.+/i',
+            'credit_card_number' => 'required|size:16|regex:/\d{16}/',
+            'credit_card_expiration_date' => 'required|size:5|date_format:y/m',
+            'credit_card_security_code' => 'required|size:3|regex:/\d{3}/',
             'account_id' => 'required|integer',
-            'amount' => 'required|numeric',
+            'amount' => 'required|numeric|regex:/^\d*\.?\d+$/',
             'allow_save' => 'present'
         ]);
 
